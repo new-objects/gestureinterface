f10a60e65bde9f44772aae5d8ce55a625617ea24
mi-skam <maksim@miskam.xyz>
2023-11-21 17:22:14 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Python development tools";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@attrs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python3.withPackages (ps:
+          with ps;
+          [
+            # Add Python packages here
+            pip
+          ]);
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            # Add other build inputs if necessary
+          ];
+        };
+      });
+}
--- src/js/scenes/Game.js
+++ src/js/scenes/Game.js
@@ -1,15 +1,39 @@
 import Phaser from 'phaser';
+import HandTracking from './game/HandTracking';
+
+// init mediapipe hand tracking
+const handTracking = new HandTracking();
 
 export default class Game extends Phaser.Scene {
+  /** @type {Phaser.Physics.Arcade.StaticBody} */
+  handLeft;
+  /** @type {Phaser.Physics.Arcade.StaticBody} */
+  handRight;
+  results;
+
   constructor() {
     super('game');
   }
 
   preload() {
     this.load.image('sky', 'assets/sky.png');
+    this.load.image('handLeft', 'assets/bird.png');
+    this.load.image('handRight', 'assets/star.png');
   }
 
   create() {
     this.add.image(400, 300, 'sky');
+    this.handLeft = this.physics.add.staticImage(200, 300, 'handLeft');
+    this.handRight = this.physics.add.staticImage(500, 300, 'handRight');
+  }
+
+  update(_, delta) {
+    // get hand tracking results
+    this.results = handTracking.getResults();
+    // update hand positions
+    // this.handLeft.x = this.results.multiHandLandmarks[0][0].x * 800;
+    // this.handLeft.y = this.results.multiHandLandmarks[0][0].y * 600;
+    // this.handRight.x = this.results.multiHandLandmarks[0][1].x * 800;
+    // this.handRight.y = this.results.multiHandLandmarks[0][1].y * 600;
   }
 }
